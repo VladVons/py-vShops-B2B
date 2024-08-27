08da72f5fb1857b2c6c7ec97bd805b53db964f3e
VladVons <vladvons@gmail.com>
2024-08-27 20:34:41 +0300

    MyCommit

---
+++ src/Task/Price/Plugin/RunApp/__init__.py
@@ -0,0 +1,22 @@
+# Created: 2024.08.27
+# Author: Vladimir Vons <VladVons@gmail.com>
+# License: GNU, see LICENSE for more details
+
+
+import os
+import asyncio
+import subprocess
+#
+from Inc.ParserX.Common import TPluginBase
+from Inc.Util.Obj import Iif
+
+
+class TRunApp(TPluginBase):
+    async def Run(self):
+        App = self.Conf['app']
+        if App.startswith('~'):
+            App = os.path.expanduser(App)
+        Cmd = [App] + self.Conf.get('args', '').split()
+        Dir = Iif('/' in App, os.path.dirname(App), None)
+        subprocess.Popen(Cmd, cwd=Dir)
+        asyncio.sleep(3)
